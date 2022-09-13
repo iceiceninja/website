4b231ac24ae44da64417c15a7421c4dac332bf96
iceiceninja <joshdbonham@gmail.com>
2022-09-13 09:30:42 -0500

    trying to fix

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const PORT = 3000;
 const express = require('express')
 const app = express()
 
-
 app.use(express.static('Public'))
 app.use('/styles', express.static(__dirname + 'Public/styles'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.get('', (req, res)=>{
+
+app.get('/', (req, res)=>{
     res.render('index')
+})
+
+app.get('/myWork', (req, res)=>{
+    res.render('myWork')
 })
 
 app.listen(PORT,()=>console.log("listening on port " + (PORT)))
